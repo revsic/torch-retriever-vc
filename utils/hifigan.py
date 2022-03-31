1aa2ae4cf25523a6e9dcf8b3f18bee7e2610eb70
revsic <revsic99@gmail.com>
2022-03-31 03:04:36 +0000

    hifigan: Add hifigan

---
+++ utils/hifigan.py
@@ -0,0 +1,45 @@
+import sys
+# ./utils/hifigan.py
+sys.path.append(f'{__file__}/../../hifi-gan')
+# hifigan generator
+from env import AttrDict
+from models import Generator
+sys.path.pop()
+
+import json
+from typing import Optional
+
+import torch
+
+
+class HiFiGANWrapper:
+    """HiFi-GAN Wrapper
+    """
+    def __init__(self, config: str, ckpt: str, device: Optional[torch.device] = None):
+        """Initializer.
+        Args:
+            config: path to the configuration.
+            ckpt: path to the checkpoint.
+            device: target computing device.
+        """
+        device = device or torch.device(
+            'cuda' if torch.cuda.is_available() else 'cpu')
+        with open(config) as f:
+            # load configuration
+            config = AttrDict(json.load(f))
+        # load checkpoint
+        ckpt = torch.load(ckpt, map_location='cpu')
+
+        self.model = Generator(config)
+        self.model.load_state_dict(ckpt['generator'])
+        self.model.to(device)
+        self.model.eval()
+
+    def forward(self, mel: torch.Tensor) -> torch.Tensor:
+        """Inference the wav.
+        Args:
+            mel: [torch.float32; [B, T, mel]], spectrogram.
+        Returns:
+            [torch.float32; [B, T x hop]], generated wav signal, in range [-1, 1]
+        """
+        return self.model(mel.transpose(1, 2))
