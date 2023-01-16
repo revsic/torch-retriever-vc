f24184474d25c015183a8c70b1f67bee6febe319
revsic <revsic99@gmail.com>
2023-01-16 14:24:34 +0000

    linguistic: Add linguistic encoder

--- retriever/config.py
+++ retriever/config.py
@@ -1,29 +1,31 @@
 class Config:
     """Retriever, module configurations.
     """
-    def __init__(self, mel: int):
-        """Initializer.
-        Args:
-            mel: size of the spectrogram.
-        """
-        self.mel = mel
-
+    def __init__(self):
         # channels
         self.contexts = 512    # context channels
         self.styles = 192      # style channels
         self.prototypes = 60   # the number of the prototypes
 
+        # linguistic
+        self.ling_kernels = 31
+        self.ling_hiddens = 128
+        self.ling_heads = 2
+        self.ling_ffn = 512
+        self.ling_blocks = 6
+        self.ling_dropout = 0.1
+
         # style retriever
-        self.ret_ffn = 512     # queries x 4 / 3
+        self.ret_ffn = 512
         self.ret_heads = 4
         self.ret_blocks = 3
         self.ret_dropout = 0.1
 
         # decoder
-        self.dec_ffn = 2048    # contexts x 4
+        self.dec_ffn = 2048
         self.dec_heads = 8
         self.dec_blocks = 4
         self.dec_dropout = 0.1
 
         # detokenizer
-        self.detok_ffn = 2048
+        self.detok_ffn = 2048  # 4096
---
+++ retriever/linguistic.py
@@ -0,0 +1,54 @@
+import torch
+import torch.nn as nn
+
+from .attention import AuxSequential, SelfAttention
+
+
+class LinguisticEncoder(nn.Module):
+    """Linguistic encoder.
+    """
+    def __init__(self,
+                 channels: int,
+                 kernels: int,
+                 hiddens: int,
+                 heads: int,
+                 ffn: int,
+                 blocks: int,
+                 dropout: float = 0.):
+        """Initializer.
+        Args:
+            channels: size of the input channels.
+            kernels: convolutional kernels.
+            hiddens: size of the hidden units.
+            heads: the number of the attention heads.
+            ffn: size of the FFN hidden channels.
+            blocks: the number of the attention blocks.
+            dropout: dropout rates for FFN.
+        """
+        super().__init__()
+        self.preconv = nn.Sequential(
+            nn.Conv1d(channels, hiddens, 1),
+            nn.ReLU(),
+            nn.Dropout(dropout),
+            # instead of positional encoding
+            nn.Conv1d(
+                hiddens, hiddens, kernels,
+                padding=kernels // 2, groups=hiddens, bias=False),
+            nn.ReLU())
+
+        self.encoder = AuxSequential(
+            SelfAttention(hiddens, heads, ffn, blocks, dropout),
+            nn.Conv1d(hiddens, hiddens, 1))
+
+    def forward(self, inputs: torch.Tensor, mask: torch.Tensor) -> torch.Tensor:
+        """Extract the linguistic informations.
+        Args:
+            inputs: [torch.float32; [B, S, channels]], input tensors.
+            mask: [torch.float32; [B, S]], sequential mask.
+        Returns:
+            [torch.float32; [B, S, hiddens]], extracted.
+        """
+        # [B, hiddens, S]
+        x = self.preconv(inputs.transpose(1, 2))
+        # [B, S, hiddens]
+        return self.encoder(x.transpose(1, 2), mask)
