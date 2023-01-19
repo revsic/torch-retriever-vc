255082c8f6378ebc111f1abf79f46d70564bbf42
revsic <revsic99@gmail.com>
2023-01-19 03:20:35 +0000

    wrapper: Simplifying

--- utils/wrapper.py
+++ utils/wrapper.py
@@ -7,6 +7,8 @@ import torch.nn.functional as F
 from config import Config
 from retriever import Retriever
 
+from .encodec import EncodecWrapper
+
 
 class TrainingWrapper:
     """Training wrapper.
@@ -21,120 +23,80 @@ class TrainingWrapper:
         self.model = model
         self.config = config
         self.device = device
-        # alias
-        self.silence = np.log(config.data.eps)
-        self.seglen = config.train.seglen
-        self.vectors = config.model.vectors
-        self.gamma = np.log(config.model.vectors)
-        self.sample = config.model.cpc_sample
+        self.encodec = EncodecWrapper(model)
 
-    def random_segment(self, bunch: List[np.ndarray]) -> np.array:
+    def random_segment(self, bunch: List[torch.Tensor]) -> Tuple[torch.Tensor, torch.Tensor]:
         """Segment the spectrogram and audio into fixed sized array.
         Args:
             bunch: input tensors.
-                mel: [np.float32; [B, T, mel]], mel-spectrogram.
-                mellen: [np.long; [B]], spectrogram lengths.
+                speeches: [np.float32; [B, T]], speeches.
+                lengths: [np.long; [B]], speech lengths.
         Returns:
-            randomly segmented spectrogram.
+            randomly segmented spectrogram and audios.
         """
-        # [B, T, mel], [B]
-        _, _, mel, _, mellen = bunch
-        # [B], min clipping
-        start = np.random.randint(np.maximum(1, mellen - self.seglen))
-        # B x [seglen, mel]
-        seg = []
-        for m, s in zip(mel, start):
-            if len(m) < self.seglen:
-                m = np.pad(m, [[0, self.seglen - len(m)], [0, 0]],
-                           constant_values=self.silence)
-            seg.append(m[s:s + self.seglen])
-        # [B, seglen, mel]
-        return np.array(seg)
+        # [B, T], [B]
+        speeches, lengths = bunch
+        # B
+        bsize, = lengths.shape
+        # [B]
+        start = torch.rand(bsize, device=self.device) * (lengths - self.seglen).clamp_min(0)
+        # [B, seglen]
+        seg = torch.stack([
+            F.pad(q[s:s + self.seglen], [0, max(self.seglen - q.shape[-1], 0)])
+            for q, s in zip(speeches, start.long())], dim=0)
+        return lengths.clamp_max(self.seglen), seg
 
-    def compute_loss(self, mel: torch.Tensor) \
+    def compute_loss(self, seg: torch.Tensor) \
             -> Tuple[torch.Tensor, Dict[str, torch.Tensor], Dict[str, torch.Tensor]]:
         """Compute the loss.
         Args:
-            mel: [torch.float32; [B, seglen, mel]], segmented spectrogram.
+            seg: [torch.float32; [B, T]], segmented speech.
         Returns:
             loss and dictionaries.
         """
-        # [B, seglen, mel], _
-        synth, aux = self.model(mel)
+        with torch.no_grad():
+            # [B, num_q, S]
+            codebook = self.encodec.forward(seg)
+            # 2 x [B, L, w2v2_channels], [B, L]
+            spk, ling, _ = self.model.wav2vec2.forward(seg)
+        # [B, L, ling_hiddens]
+        ling = self.model.linguistic.forward(ling)
+        # [B, prototypes, styles]
+        style = self.model.retriever.forward(spk)
 
-        ## 1. reconstruction
-        rec = F.l1_loss(mel, synth)
-
-        ## 2. VQ perplexity
-        # [G, V], mean across batch
-        prob = torch.softmax(aux['logits'], dim=2).mean(dim=[0, 3])
-        # [G]
-        perplexity = (prob * torch.log(prob + 1e-5)).sum(dim=1).exp() / self.vectors
+        # B, S
+        bsize, _, clen = codebook.shape
+        # [B, contexts, S]
+        ling_fst = F.interpolate(
+            self.model.proj_fst(ling).transpose(1, 2), size=clen, mode='linear')
+        # [B, S, tokens]
+        logits = self.model.dec_fst.forward(ling_fst.transpose(1, 2), style)
         # []
-        perplexity = perplexity.mean()
+        ce_fst = F.cross_entropy(
+            torch.softmax(logits.transpose(1, 2), dim=1),
+            codebook[:, 0])
 
-        ## 3. Masked lm
-        # B, T, _
-        bsize, timesteps, _ = aux['features'].shape
-        # [T]
-        mask = torch.zeros(timesteps, device=self.device).scatter(
-            0, torch.randperm(timesteps, device=self.device)[:self.sample], 1.)
-        # [B, T, C]
-        masked = aux['features'] + (
-            self.model.maskembed - aux['features']) * mask[None, :, None]
-        # [B, T, C]
-        pred = self.model.cpcpred(
-            self.model.cpcconv(masked.transpose(1, 2)).transpose(1, 2))
-        # [B, T, T], cosine similarity
-        logit = torch.matmul(
-            F.normalize(pred, dim=-1),
-            F.normalize(aux['contents'], dim=-1).transpose(1, 2))
-        # for numerical stability, torch.exp(logit - logit.max())
-        ratio = torch.exp(logit - 1.)
-
-        # [T, T]
-        pos_mask = torch.eye(timesteps, device=self.device) * mask
-        # [B, T]
-        log_pos = torch.log((ratio * pos_mask[None]).sum(dim=-1) + 1e-7)
-
-        # [T, T], exclusive candidates
-        neg_mask = mask[None] * mask[:, None] * (1 - pos_mask)
-        # [B, T]
-        neg = (ratio * neg_mask[None]).sum(dim=-1)
-
-        
-        # [], range [1, B - 1]
-        start = np.random.randint(bsize - 1) + 1
-        # [B], for shuffling
-        indices = (np.arange(bsize) + start) % bsize
-        # [B, T, T], cross-batch cossim
-        cross_logit = torch.matmul(
-            F.normalize(pred, dim=-1),
-            F.normalize(aux['contents'][indices], dim=-1).transpose(1, 2))
-        # for numerical stability
-        cross_ratio = torch.exp(cross_logit - 1.)
-        # [B, T]
-        log_neg = torch.log(neg + (cross_ratio * neg_mask[None]).sum(dim=-1) + 1e-7)
+        # alias
+        timesteps = self.config.model.timesteps
+        # [B], [0, timesteps - 1]-ranged.
+        steps = (torch.rand(bsize, device=seg.device) * timesteps).long()
+        # [timesteps - 1, embeds]
+        embeds = self.model.steps(timesteps - 1)
+        # [B, S, contexts]
+        codes = torch.stack([
+            self.model.codebooks[i](code[i])
+            for code, i in zip(codebook, steps)], dim=0)
+        # [B, S, tokens]
+        logits = self.model.dec_rst.forward(
+            codes, ling, style, steps, embeds[steps])
+        # []
+        ce_rst = F.cross_entropy(
+            torch.softmax(logits.transpose(1, 2), dim=1),
+            codebook[torch.arange(bsize), steps])
 
         # []
-        infonce = -(log_pos - log_neg).mean()
-
-        # []
-        loss = self.config.train.lambda_rec * rec + \
-            self.config.train.lambda_vq * perplexity + \
-            self.config.train.lambda_sc * infonce
+        loss = ce_fst + ce_rst
         losses = {
             'loss': loss.item(),
-            'rec': rec.item(), 'vq': perplexity.item(), 'cpc': infonce.item()}
-        return loss, losses, {'synth': synth.cpu().detach().numpy()}
-
-    def update_gumbel_temp(self):
-        """Update gumbol softmax temperature.
-        """
-        # update temperature
-        temp = max(
-            self.model.quantize.temp.item() * self.config.model.temp_factor,
-            self.config.model.temp_min)
-        # re-register buffer
-        self.model.quantize.register_buffer(
-            'temp', torch.tensor(temp, dtype=torch.float32, device=self.device))
+            'ce-fst': ce_fst.item(), 'ce-rst': ce_rst.item()}
+        return loss, losses
