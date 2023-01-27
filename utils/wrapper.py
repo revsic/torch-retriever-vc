59bd440040fc195c8b1eab6caa995b712e1bf64d
revsic <revsic99@gmail.com>
2023-01-27 03:23:34 +0000

    wrapper: Add spectrogram loss

--- utils/wrapper.py
+++ utils/wrapper.py
@@ -1,8 +1,9 @@
-from typing import Dict, List, Tuple
+from typing import Dict, Optional, Tuple
 
 import numpy as np
 import torch
 import torch.nn.functional as F
+import torchaudio
 
 from config import Config
 from retriever import Retriever
@@ -24,33 +25,51 @@ class TrainingWrapper:
         self.config = config
         self.device = device
         self.aug = Augment(config).to(device)
+        # [win] auxiliary tensors
+        self.window = torch.hann_window(config.model.win, device=device)
+        # [mel, fft // 2 + 1]
+        self.fbank = torchaudio.functional.melscale_fbanks(
+            config.model.fft // 2 + 1,
+            config.model.fmin,
+            config.model.fmax,
+            config.model.mel,
+            config.model.sr,
+            norm='slaney', mel_scale='slaney').to(device).T
         # alias
         self.seglen = self.config.train.seglen
         self.lambda_cont = self.config.train.cont_start
 
-    def random_segment(self, bunch: List[torch.Tensor]) -> Tuple[torch.Tensor, torch.Tensor]:
+    def segment(self,
+                seq: torch.Tensor,
+                len_: Optional[torch.Tensor] = None,
+                start: Optional[torch.Tensor] = None,
+                seglen: Optional[int] = None) -> Tuple[torch.Tensor, torch.Tensor]:
         """Segment the spectrogram and audio into fixed sized array.
         Args:
-            bunch: input tensors.
-                speeches: [np.float32; [B, T]], speeches.
-                lengths: [np.long; [B]], speech lengths.
+            seq: [np.float32; [B, ..., T]], sequence tensor.
+            len_: [torch.long; [B]], sequence lengths.
+            start: [torch.long; [B]], start position, randomize start position if not provided.
+            seglen: segment length, use `self.seglen` if not provided.
         Returns:
-            randomly segmented spectrogram and audios.
+            segmented sequence and start position
         """
-        # [B, T], [B]
-        speeches, lengths = bunch
-        # B
-        bsize, = lengths.shape
-        # [B]
-        start = torch.rand(bsize, device=self.device) * (lengths - self.seglen).clamp_min(0)
+        # placeholder
+        seglen = seglen or self.seglen
+        if start is None:
+            assert len_ is not None, 'either `len_` or `start` should be passed'
+            # B
+            bsize, = len_.shape
+            # [B]
+            start = torch.rand(bsize, device=self.device) * (len_ - seglen).clamp_min(0)
+            start = start.long()
         # [B, seglen]
         seg = torch.stack([
-            F.pad(q[s:s + self.seglen], [0, max(self.seglen - q.shape[-1], 0)])
-            for q, s in zip(speeches, start.long())], dim=0)
-        return lengths.clamp_max(self.seglen), seg
+            F.pad(q[..., s:s + seglen], [0, max(seglen - q.shape[-1], 0)])
+            for q, s in zip(seq, start)], dim=0)
+        return seg, start
 
-    def compute_loss(self, seg: torch.Tensor) \
-            -> Tuple[torch.Tensor, Dict[str, torch.Tensor]]:
+    def compute_loss(self, speeches: torch.Tensor, lengths: torch.Tensor) \
+            -> Tuple[torch.Tensor, Dict[str, float], Dict[str, np.ndarray]]:
         """Compute the loss.
         Args:
             seg: [torch.float32; [B, T]], segmented speech.
@@ -58,71 +77,51 @@ class TrainingWrapper:
             loss and dictionaries.
         """
         with torch.no_grad():
-            # [B, num_q, S]
-            codebook = self.model.encodec.forward(seg)
-            # [B, S]
-            aug1 = self.aug.augment(seg)
-            aug2 = self.aug.augment(seg)
-            # 2 x [B, L, w2v2_channels], [B, L]
-            spk, ling, _ = self.model.wav2vec2.forward(aug1)
+            # [B, fft // 2 + 1, T // hop]
+            spec = torch.stft(
+                speeches,
+                self.config.model.fft,
+                self.config.model.hop,
+                self.config.model.win,
+                self.window,
+                return_complex=True).abs()
+            # [B, mel, T // hop]
+            mel = torch.matmul(self.fbank, spec).clamp_min(1e-5).log()
+            # T // hop
+            _, _, mlen = mel.shape
+            # [B]
+            mellen = (lengths / self.config.model.hop).ceil().long().clamp_max(mlen)
+            # [B, mel, seglen // hop]
+            hop = self.config.model.hop
+            mel, start = self.segment(mel, mellen, seglen=self.seglen // hop)
+            # [B, seglen]
+            seg, _ = self.segment(speeches, start=start * hop)
             # [B, L, w2v2_channels]
-            _, ling2, _ = self.model.wav2vec2.forward(aug2)
+            spk, _, _ = self.model.wav2vec2.forward(seg)
+
+            # [B x 2, seglen]
+            aug = self.aug.augment(seg.repeat(2, 1)).chunk(2, dim=0)
+            # [B x 2, L, w2v2_channels]
+            _, ling, _ = self.model.wav2vec2.forward(aug)
+            # [B, L, w2v2_channels]
+            ling1, ling2, = ling.chunk(2, dim=0)
+        
         # [B, L, ling_hiddens]
-        ling = self.model.linguistic.forward(ling)
+        ling = self.model.linguistic.forward(ling1)
         # [B, prototypes, styles]
         style = self.model.retriever.forward(spk)
 
-        quantizers = self.model.quantizers
-        quant_fst, *_ = quantizers
+        # [B, L, contexts]
+        states = self.model.proj_context(ling)
+        # [B, contexts, S]
+        states = F.interpolate(states.transpose(1, 2), size=mlen, mode='nearest')
         # [B, S, contexts]
-        codes = self.model.embedding(quant_fst, codebook[:, 0])
-        # [B, S, contexts], assign start token
-        codes = F.pad(codes, [0, 0, 1, -1])
-        codes[:, 0] = self.model.start
-        # B
-        bsize, _, _ = codebook.shape
-        # [1, E], placeholder
-        null = torch.zeros(1, self.config.model.embeds, device=self.device)
-        # [B, S, tokens]
-        logits = self.model.dec_fst.forward(codes, ling, style, 0, null)
-        # []
-        ce_fst = F.cross_entropy(logits.transpose(1, 2), codebook[:, 0])
-        # metric purpose
-        metric_fst = (logits.argmax(dim=-1) == codebook[:, 0]).float().mean().item()
+        states = self.model.decoder.forward(states.transpose(1, 2), style)
+        # [B, S, mel]
+        pred = self.model.detok(states)
 
-        # alias
-        timesteps = self.config.model.timesteps
-        # [B], [0, timesteps - 1)
-        steps = (torch.rand(bsize, device=self.device) * (timesteps - 1)).long()
-        # [timesteps - 1, embeds]
-        embeds = self.model.steps(timesteps - 1)
-
-        # [B, S, contexts]
-        codes = torch.stack([
-            torch.stack([
-                self.model.embedding(quantizers[j], code[j])
-                for j in range(i + 1)], dim=0).sum(dim=0)
-            for code, i in zip(codebook, steps.tolist())], dim=0)
-        # [B, S, tokens]
-        logits = self.model.dec_rst.forward(
-            codes, ling, style, steps, embeds[steps])
-        # [B]
-        aranger = torch.arange(bsize, device=self.device)
         # []
-        ce_rst = F.cross_entropy(
-            logits.transpose(1, 2), codebook[aranger, steps + 1])
-        # metric purpose
-        with torch.no_grad():
-            # [B]
-            acc_rst = (logits.argmax(dim=-1) == codebook[aranger, steps + 1]).float().mean(dim=-1)
-            # []
-            metric_rst = acc_rst.mean().item()
-            # step-wise aggregation
-            steps = steps.tolist()
-            agg = {s: [] for s in steps}
-            for s, a in zip(steps, acc_rst.tolist()):
-                agg[s].append(a)
-            metric_agg = {s + 1: sum(a) / max(len(a), 1) for s, a in agg.items()}
+        rctor = F.l1_loss(mel.transpose(1, 2), pred)
 
         # [2, B, L, ling_hiddens]
         ling_ex = torch.stack([
@@ -162,19 +161,17 @@ class TrainingWrapper:
         metric_neg = ((confusion * mask).sum(dim=-1) / n_cand).mean().item() * kappa
 
         # []
-        loss = ce_fst + ce_rst + self.lambda_cont * cont_loss
+        loss = rctor + self.lambda_cont * cont_loss
         losses = {
             'loss/loss': loss.item(),
-            'loss/ce-fst': ce_fst.item(),
-            'loss/ce-rst': ce_rst.item(),
             'loss/cont': cont_loss.item(),
             'metric/neg': metric_neg,
             'metric/pos': metric_pos,
-            'metric/fst': metric_fst,
-            'metric/rst': metric_rst,
             'common/warmup': self.lambda_cont}
-        losses.update({f'metric-aux/step{i}': acc for i, acc in metric_agg.items()})
-        return loss, losses
+        return loss, losses, {
+            'seg': seg.cpu().detach().numpy(),
+            'mel': mel.transpose(1, 2).cpu().detach().numpy(),
+            'rctor': pred.cpu().detach().numpy()}
 
     def update_warmup(self):
         """Update the content loss weights.
