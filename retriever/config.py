f0608e1a00730f5cbc822ab99c79a61fbb95f828
revsic <revsic99@gmail.com>
2023-01-27 01:16:58 +0000

    config: Add spectrogram configurations

--- retriever/config.py
+++ retriever/config.py
@@ -1,24 +1,22 @@
 class Config:
     """Retriever, module configurations.
     """
-    # hardcoded
-    ENCODEC_STRIDES = 320
-    ENCODEC_DIM = 128
-
     def __init__(self):
-        self.sr = 24000
+        self.sr = 22050
         self.w2v2_name = 'facebook/wav2vec2-large-xlsr-53'
 
         # channels
         self.contexts = 512    # context channels
         self.styles = 192      # style channels
         self.prototypes = 60   # the number of the prototypes
-        self.encodecs = 1024
 
-        # time embeddings
-        self.pe = 128
-        self.timesteps = 8
-        self.embeds = 512
+        # spec
+        self.mel = 80
+        self.hop = 256
+        self.win = 1024
+        self.fft = 1024
+        self.fmin = 0
+        self.fmax = 8000
 
         # linguistic
         self.ling_hiddens = 256
@@ -34,12 +32,7 @@ class Config:
         self.ret_dropout = 0.1
 
         # decoder
-        self.fst_heads = 4
-        self.fst_ffn = 2048
-        self.fst_blocks = 12
-        self.fst_dropout = 0.5
-
-        self.rst_heads = 4
-        self.rst_ffn = 2048
-        self.rst_blocks = 12
-        self.rst_dropout = 0.5
+        self.dec_heads = 4
+        self.dec_ffn = 2048
+        self.dec_blocks = 12
+        self.dec_dropout = 0.1
